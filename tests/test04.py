5599d0f77d008446a038c9ec036730158b37dd03
mykespb <mykespb@gmail.com>
2019-01-23 12:34:58 +0300

    better dates

--- tests/test04.py
+++ tests/test04.py
@@ -1,22 +1,16 @@
 #! /usr/bin/env python3
-# myke 2019-01-22 1.1
+# myke 2019-01-23 1.2
 
 # ------------------------- my date class
 
 import datetime
 
 class Mydate:
-    def __init__ (self, d=-1, m=-1, y=-1):
+    def __init__ (self, d=0, m=0, y=0):
         _today = datetime.date.today()
-        if (d == -1):
-            d = _today.day
-        if (m == -1):
-            m = _today.month
-        if (y == -1):
-            y = _today.year
-        self.year = y
-        self.month = m
-        self.day = d
+        self.year = y if y else _today.year
+        self.month = m if m else _today.month
+        self.day = d if d else _today.day
 
     def __str__(self):
         return f"{self.year:04}-{self.month:02}-{self.day:02}"
