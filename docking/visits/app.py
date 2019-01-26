5062ef76b88a4fd535f779c4a1938bca04c16ba0
mykespb <mykespb@gmail.com>
2019-01-26 20:41:03 +0300

    1st visit app, flask, redis, dc

---
+++ docking/visits/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# python3 + redis test app with docker-compose
+# myke 2019-01-26 1.1
+
+from flask import Flask
+from redis import Redis
+
+app = Flask (__name__)
+redis = Redis (host='redis', port=6379)
+
+@app.route ('/')
+def hello ():
+    count = redis.incr ('visits')
+    return 'Hello my friend! Visit #{}.'.format(count)
+
+if __name__ == "__main__":
+    app.run (host="0.0.0.0", debug=True)
+
