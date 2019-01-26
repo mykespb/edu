0a4983402e74c28d7ba2967b2c4b82feb4f9036d
mykespb <mykespb@gmail.com>
2019-01-27 00:37:15 +0300

    better order of layers

--- docking/visits/app.py
+++ docking/visits/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # python3 + redis test app with docker-compose
-# myke 2019-01-26 1.1
+# myke 2019-01-27 1.2
 
 from flask import Flask
 from redis import Redis
@@ -11,7 +11,7 @@ redis = Redis (host='redis', port=6379)
 @app.route ('/')
 def hello ():
     count = redis.incr ('visits')
-    return 'Hello my friend! Visit #{}.'.format(count)
+    return 'Hello my dear friend! Visit #{}.'.format(count)
 
 if __name__ == "__main__":
     app.run (host="0.0.0.0", debug=True)
