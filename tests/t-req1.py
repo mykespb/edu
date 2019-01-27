87f4de3378ec30b7419ec3983d63e84b138e9724
mykespb <mykespb@gmail.com>
2019-01-27 05:32:28 +0300

    better desc for web getter

--- tests/t-req1.py
+++ tests/t-req1.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # testing requests library
 # python3, requests (virtualenv)
-# myke 2019-01-24
+# myke 2019-01-24 1.1
 
 import requests
 import re
@@ -32,3 +32,21 @@ def main():
     print ("all done.\n")
 
 main()
+
+# ~ URL   = http://www.myke.spb.ru
+# ~ Title = Main webpage of Myke
+# ~ step done.
+
+# ~ URL   = https://www.youtube.com
+# ~ Title = YouTube
+# ~ step done.
+
+# ~ URL   = https://www.youtube.com/watch?v=O4zlCn-rwJM
+# ~ Title = Cirrus Owner: Ken Griffey, Jr. - YouTube
+# ~ step done.
+
+# ~ URL   = https://www.youtube.com/watch?v=Fne7Quz3oGs
+# ~ Title = Как стать программистом? Настройка тестирования Pytest Стрим за 9 минут - YouTube
+# ~ step done.
+
+# ~ all done.
