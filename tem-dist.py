3f523788ef79cfbe31e1d93b9eb3d0cf70825070
mykespb <mykespb@gmail.com>
2020-05-25 00:04:50 +0300

    reads pattrens

--- tem-dist.py
+++ tem-dist.py
@@ -11,13 +11,22 @@
 __version__ = "0.1"
 __date__    = "2020-05-24"
 
+import os, os.path
+
+cwd = os.getcwd()
+
+tpl_file = 'tem-dist.tpl'
 tpls = []
+no_tpl = 0
+
 files = []
+
 
 def main(args):
     """ main dispatcher """
 
     print (f"This is tem-dist, ver. {__version__} of {__date__}")
+    print (f"current directoory is {cwd}")
     get_tpl()
     get_files()
     scan_all()
@@ -25,6 +34,23 @@ def main(args):
     return 0
 
 def get_tpl():
+    global no_tpl
+    if (os.path.isfile (tpl_file)):
+        print (f"file {tpl_file} exists ok, working")
+        with open (tpl_file) as tplfile:
+            for tplka in tplfile:
+                tpl = tplka.strip()
+                if tpl.startswith("#"): continue
+                if len(tpl) == 0: continue
+
+                no_tpl += 1
+                pat, dir = tpl.split(maxsplit=1)
+                print (no_tpl, tpl, "=>", pat, dir)
+
+
+    else:
+        print (f"file {tpl_file} does not exist, quitting")
+        raise "no template file"
     pass
 
 def get_files():
