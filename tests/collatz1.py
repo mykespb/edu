519116224153150f98fe95abc96f9e11548c8a9b
mykespb <mykespb@gmail.com>
2021-08-06 21:45:14 +0300

    commented for good sources

--- tests/collatz1.py
+++ tests/collatz1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3.9
-# Mikhail Kolodin 2021-08-04 1.1
+# Mikhail Kolodin 2021-08-04 2021-08-06 1.2
 
 # solving Collatz problem... showing it :)
 # https://ru.wikipedia.org/wiki/%D0%93%D0%B8%D0%BF%D0%BE%D1%82%D0%B5%D0%B7%D0%B0_%D0%9A%D0%BE%D0%BB%D0%BB%D0%B0%D1%82%D1%86%D0%B0
 # https://www.popmech.ru/science/510082-10-slozhneyshih-matematicheskih-zadach-kotorye-ostayutsya-nereshennymi/
+# https://www.youtube.com/watch?v=094y1Z2wpJg The Simplest Math Problem No One Can Solve
+#    8 312 054 просмотра30 июл. 2021 г
+# I tested it up to 1 million
 
 LIM = 1_000
 
