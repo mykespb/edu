10357ec9f1f96001a026ba54605a0df15eb913b9
mykespb <mykespb@gmail.com>
2021-08-06 21:42:30 +0300

    better collatz - counted up to million

--- tests/collatz.py
+++ tests/collatz.py
@@ -5,7 +5,7 @@
 # https://ru.wikipedia.org/wiki/%D0%93%D0%B8%D0%BF%D0%BE%D1%82%D0%B5%D0%B7%D0%B0_%D0%9A%D0%BE%D0%BB%D0%BB%D0%B0%D1%82%D1%86%D0%B0
 # https://www.popmech.ru/science/510082-10-slozhneyshih-matematicheskih-zadach-kotorye-ostayutsya-nereshennymi/
 
-LIM = 200
+LIM = 1_000
 
 done = set()
 
