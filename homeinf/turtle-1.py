229f87e1493fa5feb1fd865ef7db1bbe1e5d2884
mykespb <mykespb@gmail.com>
2021-12-07 01:04:22 +0300

    черепашка 1-3

--- homeinf/turtle-1.py
+++ homeinf/turtle-1.py
@@ -5,16 +5,26 @@
 # turtle-1.py
 
 # ~ простые рисунки черепашкой
+# ~ работает и можно ещё много экспериментировать
 
-import random
+# ~ https://realpython.com/beginners-guide-python-turtle/
+# ~ https://docs.python.org/3/library/turtle.html
+# ~ https://gvard.github.io/py/turtle/
+
+# ~ https://www.youtube.com/watch?v=Z6FKGQcjTOk
+# ~ Getting Started With Turtle in Python
+# ~ 1 788 просмотров17 дек. 2020 г.
+
+from random import randint as ri
 import turtle
 
 t = turtle.Turtle()
 
 t.penup()
 t.setpos(0, 0)
-t.width(10)
+t.width(2)
 t.pendown()
+t.speed(10)
 
 def pic1():
 
@@ -44,9 +54,18 @@ def pic2():
         t.left(90)
 
 def pic3():
-    ...
+    for cir in range(10):
+        x = ri(-450, 450)
+        y = ri(-450, 450)
+        r = ri(35, 55)
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+        t.circle(r)
 
-pic2()
+pic1()
+# ~ pic2()
+# ~ pic3()
 
 turtle.update()
 turtle.done()
