875344632f4fa99a7917c43099737b542c73d412
mykespb <mykespb@gmail.com>
2021-12-18 21:22:35 +0300

    more exercises and turtles

---
+++ homeinf/mk-turtle01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.10
+# Mikhail (myke) Kolodin, 2021
+# 2021-12-18 2021-12-18 1.0
+# mk-turtle-01.py
+
+import random, turtle
+
+LIMIT = 30
+
+dirs = [90*n for n in range(4)]
+#dirs = [30*n for n in range(12)]
+#dirs = [45*n for n in range(8)]
+colors = "green yellow navy silver gray red white blue cyan black".split()
+
+t = turtle.Turtle()
+t.width(5)
+
+for step in range(LIMIT):
+	if random.choice((True, False)):
+		dir = random.choice(dirs)
+		t.right(dir)
+	far = random.randint(10, 50)
+	col = random.choice(colors)
+	t.color(col)
+	t.fd(far)
+	
+input()
