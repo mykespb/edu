f7c00058fb0442fa1903998801e822d625c61c44
mykespb <mykespb@gmail.com>
2022-01-07 23:02:15 +0300

    new app test edu ipo 1

--- tests/t-ipo.py
+++ tests/t-ipo.py
@@ -5,3 +5,26 @@
 # 2022-01-07 2022-01-07 0.1
 
 # test input - process - output scheme
+
+def in():
+    """ ввести данные
+    """
+    ...
+
+def proc():
+    """ обработать данные
+    """
+    ...
+
+def out():
+    """ вывести данные
+    """
+    ...
+
+def main():
+    """ диспетчер
+    """
+    if in() and proc() and out():
+        print("всё готово")
+
+main()
