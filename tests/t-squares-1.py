67ce89155a797b0c14b0e591003a2a561fe3562e
mykespb <mykespb@gmail.com>
2022-01-08 00:09:57 +0300

    квадраты с заданными сторонами

---
+++ tests/t-squares-1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# t-squares-1-1.py
+# (C) Mikhail (myke) Kolodin, 2021
+# 2022-01-08 2022-01-08 1.0
+
+# ~ нарисовать рамку квадрата с заданной стороной
+# ~ нарисовать несколько таких квадратов
+
+
+def square(size):
+    """рисуем 1 квадрат со стороной size"""
+
+    if size<1:
+        print()
+        return
+    elif size==1:
+        print("*\n")
+        return
+
+    stars  = size * "*"
+    blanks = "*" + (size-2) * " " + "*"
+
+    print(stars)
+    for line in range(size-2):
+        print(blanks)
+    print(stars)
+    print()
+
+def main():
+    """главная - запуск тестов"""
+
+    for test in range(6):
+        square(test)
+
+
+main()
+
+
+# ~ *
+
+# ~ **
+# ~ **
+
+# ~ ***
+# ~ * *
+# ~ ***
+
+# ~ ****
+# ~ *  *
+# ~ *  *
+# ~ ****
+
+# ~ *****
+# ~ *   *
+# ~ *   *
+# ~ *   *
+# ~ *****
