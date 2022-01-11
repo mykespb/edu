9bc1f6c561bddf22c115cc0d7222bcaa85b02116
mykespb <mykespb@gmail.com>
2022-01-11 18:12:02 +0300

    доделана задачка

--- homeinf/long-strings.py
+++ homeinf/long-strings.py
@@ -1,9 +1,13 @@
 #!python3.10
 
 # Mikhail (myke) Kolodin, 2021
-# 2021-11-25 2021-11-25 1.0
+# 2021-11-25 2022-01-11 1.0
 # long-strings.py
+
 # упражнения с длинными строками
+# ~ в  каждой строке найти самое короткое и самое длинное слово
+# ~ учитываются только буквы, без знаков препинания
+# ~ и кириллица, и латиница
 
 ls = """Какие нервные лица - быть беде;
 Я помню, было небо, я не помню где;
@@ -15,7 +19,35 @@ ls = """Какие нервные лица - быть беде;
 Рок-н-ролл мертв, а я еще нет.
 """
 
-print (ls)
+alfa = "abcdefghijklmnopqrstuvwxyz" + "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+chars = alfa + " "
+
+# ~ print (ls)
 
 for i, l in enumerate(ls.split("\n")):
-    print (i, l)
+    # ~ print (i, l)
+    l = l.strip()
+    if not l:
+        continue
+
+    s = ""
+    for c in l.lower():
+        if c in chars:
+            s += c
+
+    print(s)
+
+    longlen = shortlen = -1
+    shortword = longword = ""
+
+    for word in s.split():
+        alen = len(word)
+        if longlen == -1 or alen > longlen:
+            longlen = alen
+            longword = word
+        if shortlen == -1 or alen < shortlen:
+            shortlen = alen
+            shortword = word
+
+    print("короткое:", shortword, ", букв:", shortlen)
+    print("длинное: ", longword,  ", букв:", longlen)
