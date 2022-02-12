1066d99cdd2a492a0815abc5420c07e45ce226de
mykespb <mykespb@gmail.com>
2022-02-12 18:40:21 +0300

    void

---
+++ homeinf/primes1.py
@@ -0,0 +1,20 @@
+# primes1.py (c) Mikhail Kolodin
+# 2022-02-12 2022-02-12 1.0
+
+import math
+
+# определение функции
+def primes(limit=100):
+    """печать простых до limit"""
+    for test in range(2, limit+1):
+        for tester in range(2, math.isqrt(test)):
+            if test % tester == 0:
+                break
+        else:
+            print(test, end=", ")
+
+# вызов функции
+#primes()
+# должно напечатать простые до 100
+primes(1000)
+# должно напечатать простые до 1000
