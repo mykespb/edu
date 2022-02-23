8f0efa5796d920770f2c42c52bb2fbfa58f6010e
mykespb <mykespb@gmail.com>
2022-02-23 19:01:34 +0300

    1-100 без цифр

---
+++ homeinf/bezcifr.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2022
+# 2022-02-23 2022-02-23 1.0
+# bezcifr.py
+
+# ~ Считаем до 100…
+
+# ~ Часть 1. 
+# ~ Напечатать натуральные числа до 100 включительно через запятую.
+
+def ver_aa():
+    """просто 1..100"""
+
+    for i in range(1, 101):
+        print(i, end=", ")
+
+# ~ ver_aa()
+
+def ver_ab():
+    """просто 1..100"""
+
+    print(*(i for i in range(1, 101)), sep=", ")
+
+# ~ ver_ab()
+
+# ~ Часть 2. 
+# ~ Напечатать натуральные числа до 100 включительно через запятую.
+# ~ не используя в программе ни одной цифры.
+
+import math
+
+def ver_ba():
+    """и без цифр"""
+
+    zero     = int()
+    one      = int(round(math.e / math.pi))
+
+    szero    = str(zero)
+    sone     = str(one)
+    sstoodin = sone + szero + sone
+    stoodin  = int(sstoodin)
+
+    for i in range(one, stoodin):
+        print(i, end=", ")
+
+# ~ ver_ba()
+
+def ver_bb():
+    """и без цифр"""
+
+    zero = int()
+    one  = int(round(math.cos(zero)))
+    two  = one + one
+
+    mdg = int(math.degrees(one) - math.tau) * two - one
+
+    for i in range(one, mdg):
+        print(i, end=", ")
+
+# ~ ver_bb()
+
+def ver_bc():
+    """и без цифр"""
+
+    sa = len("a")
+    sb = len("aaaaaaaaaa")
+    sb *= sb
+    sb += sa
+
+    for i in range(sa, sb):
+        print(i, end=", ")
+
+ver_bc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
