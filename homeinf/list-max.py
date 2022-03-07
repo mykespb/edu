e285a10e6fbea76904ea27842ef83da3e1350c72
mykespb <mykespb@gmail.com>
2022-03-07 22:36:12 +0300

    списки - макс и мин

--- homeinf/list-max.py
+++ homeinf/list-max.py
@@ -5,3 +5,18 @@
 # list-max.py
 
 # ~ Найти макс и мин в списке (без использования встроенных функций max, min).
+
+import random
+spisok = [random.randint(-100, 100) for _ in range(10)]
+
+print("исходный список:", spisok)
+
+amin = amax = spisok[0]
+
+for e in spisok[1:]:
+    if e > amax:
+        amax = e
+    if e < amin:
+        amin = e
+
+print("минимум:", amin, ", максимум:", amax)
