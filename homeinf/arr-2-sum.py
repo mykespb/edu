3150a85be93996574f22224840a79057ed3f4fb9
mykespb <mykespb@gmail.com>
2022-03-17 22:08:12 +0300

    test from amazon with array, ok

--- homeinf/arr-2-sum.py
+++ homeinf/arr-2-sum.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2022
-# 2022-03-17 2022-03-17 1.0
+# 2022-03-17 2022-03-17 1.1
 # arr-2-sum.py
 
 # ~ Дан список с упорядоченныи целыми числами и ещё одно целое число.
 # ~ Найти пару чисел в списке, сумма которых равна этому числу,
-# ~ либо сказать, что такоих чисел нет.
+# ~ либо сказать, что такоих чисел нет;
+# ~ если решений несколько, показать одно любое.
 
 # ~ В нашем случае надо воспользоваться доп. функцией give_me_task(),
 # ~ которая сгенерирует случайное задание.
+
+# --------------- готовим -----------------
 
 from random import randint, shuffle
 
@@ -26,7 +29,7 @@ def give_me_task(diap=10, lmin=5, lmax=15):
     shuffle(arr)
     arr.sort()
 
-    if randint(0, 9):
+    if not randint(0, 9):
         num += 1
 
     return arr, num
@@ -39,4 +42,21 @@ def tests(times=5):
     for i in range(times):
         print(give_me_task())
 
-tests()
+# ~ tests()
+
+# --------------- решаем -----------------
+
+def ver1():
+    """попытка 1"""
+
+    arr, num = give_me_task()
+    print("список:", arr, ", число:", num)
+
+    for i in range(len(arr)):
+        if (num - arr[i]) in (arr[:i] + arr[i+1:]):
+            print("решение: ", arr[i], ", ", num-arr[i])
+            break
+    else:
+        print("нет решений")
+
+ver1()
