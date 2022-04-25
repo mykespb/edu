7c61dcb8adc6a0cbf00431059387546d56b9d597
mykespb <mykespb@gmail.com>
2022-04-25 19:52:33 +0300

    dates conversion 3

--- homeinf/dates-conv.py
+++ homeinf/dates-conv.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
-# myke 2022-04-25 2022-04-25 1.2
+# myke 2022-04-25 2022-04-25 1.3
 
 # ~ конвертер дат 
 # ~ есть 2 формата YYYY-MM-DD и DD.MM.YYYY
 # ~ получив строку с датой в одном из этих форматов, 
 # ~ перевести её в другой из этих- форматов и вернуть как результат
 
-def conv1(di):
+# программа
+def conv(di):
     if '-' in di:
         return ".".join(di.split('-')[::-1])
     else:
         return "-".join(di.split('.')[::-1])
 
+
+# проверка
 dates = "20.03.1968 1946-01-21 01.07.1946 29.04.2010" . split()
 
 for adate in dates:
-    print(adate, "-->", conv1(adate))
+    print(adate, "-->", conv(adate))
 
 
 # ~ 20.03.1968 --> 1968-03-20
