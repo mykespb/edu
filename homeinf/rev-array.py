8e43f792856640cfaf6130ea6a82aa04683518f1
mykespb <mykespb@gmail.com>
2022-05-01 14:52:53 +0300

    while swap

--- homeinf/rev-array.py
+++ homeinf/rev-array.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2022
-# 2022-04-08 2022-04-15 1.1
+# 2022-04-08 2022-05-01 2.0
 # rev-array.py
 
 from random import randint as ri
 a = [ri(-100, 100) for _ in range(10)]
+
+print("\n\n-------------------- version 1 ---------------------\n\n")
 
 # правильно
 print(a)
@@ -29,3 +31,23 @@ for i in range(lena):
 print(a)
 # ~ [20, 92, 2, 44, -81, -29, 70, 66, -71, -23]
 # ~ [20, 92, 2, 44, -81, -29, 70, 66, -71, -23]
+
+print("\n\n-------------------- version 2 ---------------------\n\n")
+
+print(a)
+
+left  = 0
+right = len(a)-1
+
+while left < right:
+    a[left], a[right] = a[right], a[left]
+    left  += 1
+    right -= 1
+
+print(a)
+
+# ~ -------------------- version 2 ---------------------
+
+
+# ~ [-94, -69, 36, -90, 26, 23, -97, 57, -79, 20]
+# ~ [20, -79, 57, -97, 23, 26, -90, 36, -69, -94]
