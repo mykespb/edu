be3180105057ba0a1fce6cdb203f1415135af616
mykespb <mykespb@gmail.com>
2022-05-15 21:47:19 +0300

    подсчёт чисел на переменных

---
+++ homeinf/howmany-vars.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2022
+# 2022-04-15 2022-04-15 1.1
+# howmany-vars.py
+
+# ~ ДЗ2. Сколько каких?
+# ~ Дан случайный список вида
+# ~ from random import randint as ri
+# ~ a = [ri(0, 10) for _ in range(10)]
+# ~ Определить, сколько каких чисел в нём.
+
+from random import randint as ri
+
+# задание
+a = [ri(0, 10) for _ in range(10)]
+print("вход:", a, "\n")
+
+# решение на отдельных переменных
+n0 = n1 = n2 = n3 = n4 = n5 = n6 = n7 = n8 = n9 = n10 = 0
+
+print("число сколько")
+
+for e in a:
+    if e == 0:
+        n0 += 1
+    elif e == 1:
+        n1 += 1
+    elif e == 2:
+        n2 += 1
+    elif e == 3:
+        n3 += 1
+    elif e == 4:
+        n4 += 1
+    elif e == 5:
+        n5 += 1
+    elif e == 6:
+        n6 += 1
+    elif e == 7:
+        n7 += 1
+    elif e == 8:
+        n8 += 1
+    elif e == 9:
+        n9 += 1
+    elif e == 10:
+        n10 += 1
+
+# печать результата
+if n0 > 0:
+    print(f"{0:3}   {n0:4}")
+if n1 > 0:
+    print(f"{1:3}   {n1:4}")
+if n2 > 0:
+    print(f"{2:3}   {n2:4}")
+if n3 > 0:
+    print(f"{3:3}   {n3:4}")
+if n4 > 0:
+    print(f"{4:3}   {n4:4}")
+if n5 > 0:
+    print(f"{5:3}   {n5:4}")
+if n6 > 0:
+    print(f"{6:3}   {n6:4}")
+if n7 > 0:
+    print(f"{7:3}   {n7:4}")
+if n8 > 0:
+    print(f"{8:3}   {n8:4}")
+if n9 > 0:
+    print(f"{9:3}   {n9:4}")
+if n10 > 0:
+    print(f"{10:3}   {n10:4}")
+
