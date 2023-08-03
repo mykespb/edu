1ac423bf4c7a08f501278f28378071abe5c91b9d
mykespb <mykespb@gmail.com>
2023-08-03 17:20:27 +0300

    find sole number, v.3

--- tests/find1.py
+++ tests/find1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# find1.py 2023-08-03 2023-08-03 2.1
+# find1.py 2023-08-03 2023-08-03 3.0
 # (C) Mikhail Kolodin, 2023
 
 # Дан список целых чисел.
@@ -17,6 +17,7 @@ def find1(arr: list[int]) -> int:
 
     return times.pop()
 
+
 def find2(arr: list[int]) -> int:
     """find 1, ver.2"""
     times = set()
@@ -27,16 +28,29 @@ def find2(arr: list[int]) -> int:
     return times.pop()
 
 
+def find3(arr: list[int]) -> int:
+    """find 1, ver.2"""
+
+    res = 0    
+    for n in arr:
+        res ^= n
+
+    return res
+
+
 def test(arr: list[int]) -> int:
     """run 1 test"""
 
     print(f"{arr=} -> {find1(arr)}")
     print(f"{arr=} -> {find2(arr)}")
+    print(f"{arr=} -> {find3(arr)}")
 
 test([1, 2, 2, 8, 1, 8, 3, 4, 4])
 test([1])
 
-# ~ arr=[1, 2, 2, 8, 1, 8, 3, 4, 4] -> 3
-# ~ arr=[1, 2, 2, 8, 1, 8, 3, 4, 4] -> 3
-# ~ arr=[1] -> 1
-# ~ arr=[1] -> 1
+# arr=[1, 2, 2, 8, 1, 8, 3, 4, 4] -> 3
+# arr=[1, 2, 2, 8, 1, 8, 3, 4, 4] -> 3
+# arr=[1, 2, 2, 8, 1, 8, 3, 4, 4] -> 3
+# arr=[1] -> 1
+# arr=[1] -> 1
+# arr=[1] -> 1
