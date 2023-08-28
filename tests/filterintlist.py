b311dab42ee6334af2c59352ff91c0a32244be2a
mykespb <mykespb@gmail.com>
2023-08-28 20:45:35 +0300

    filter list of ints, unordered, ok

---
+++ tests/filterintlist.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python
+
+# Mikhail Kolodin, filterintlist.py, 2023-08-28, 2023-08-23, 1.0
+
+# Filter array with integers, deleting repeating values,
+# but preserving order of elements.
+
+loi =  [1, 3, 6, 3, 0, 11, 0, 4]
+lois = [1, 3, 6, 0, 11, 4]
+
+def solve(arr: list[int]) -> list[int]:
+    """make new list"""
+
+    res = {}
+
+    for e in arr:
+        if e not in res:
+            res[e] = 1
+
+    return list(res)
+
+print(loi, "=>", sol := solve(loi), "=>", sol == lois)
+
+# ~ [1, 3, 6, 3, 0, 11, 0, 4] => [1, 3, 6, 0, 11, 4] => True
