a1fd3ed48483d51b14f608a3a06c17cd9efe4a8f
mykespb <mykespb@gmail.com>
2024-07-05 19:13:53 +0300

    better barcode

--- tests/t-barcode.py
+++ tests/t-barcode.py
@@ -2,28 +2,25 @@
 
 # calculate control digit for ISBN
 # Mikhail Kolodin, 2024
-# 2024-07-04 2024-70-05 2.0
+# 2024-07-04 2024-70-05 2.1
 # https://w.wiki/ABfs
 
-def cd1(n):
+def cd1(number):
     """method 1"""
 
-    if type(n) == str:
-        n = n.replace("-", "")
-        n = int(n, 0)
+    if type(number) == str:
+        number = number.replace("-", "")
+        number = int(number, 0)
 
-    pos = 2
+    position = 2
     summa = 0
-    while n:
-        ld = n % 10
-        n //= 10
+    while number:
+        last_digit = number % 10
+        number //= 10
 
-        term = pos * ld
+        term = position * last_digit
         summa += term
-
-        #print(f"{ld=}, {n=}, {pos=}, {term=}, {summa=}")
-
-        pos += 1
+        position += 1
 
     summa %= 11
     summa = 11 - summa
@@ -31,39 +28,39 @@ def cd1(n):
     return summa
 
 
-def cd2(n):
+def cd2(number):
     """method 2"""
     
-    if type(n) == str:
-        n = n.replace("-", "")
-        n = int(n, 0)
+    if type(number) == str:
+        number = number.replace("-", "")
+        number = int(number, 0)
 
     summa = 0
-    mult = 3
+    factor = 3
 
-    while n:
-        ld = n % 10
-        n //= 10
+    while number:
+        last_digit = number % 10
+        number //= 10
 
-        ld *= mult
-        mult = 4 - mult
-        summa += ld
+        last_digit *= factor
+        factor = 4 - factor
+        summa += last_digit
 
     summa %= 10
 
-    return summa if summa == 0 else 10 - summa
+    return 10 - summa if summa else 0
 
 
-def test(n):
+def test(number):
     """test for a number"""
 
-    print(n, "-> method 1: ", cd1(n), ", method 2: ", cd2(n))
+    print(number, "-> method 1: ", cd1(number), ", method 2: ", cd2(number))
 
 
 test(226611156)
-# -> 6, 8
 test("978-5-93286161")
-# -> 1, 5
 
+# 226611156 -> method 1:  6 , method 2:  8
+# 978-5-93286161 -> method 1:  1 , method 2:  5
 
 #https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D0%B6%D0%B4%D1%83%D0%BD%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9_%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BD%D0%B8%D0%B6%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80
