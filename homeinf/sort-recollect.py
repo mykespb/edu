e6616a25dbd7cb90cda6c2f067bc3f6ba6292823
mykespb <mykespb@gmail.com>
2024-10-05 21:05:00 +0300

    sorting terrible

---
+++ homeinf/sort-recollect.py
@@ -0,0 +1,17 @@
+#!isr/bin/env python
+# Mikhail Kolodin, 2024
+# 2024-10-05 2024-10-05 1.0
+# list sorter by recollectin array
+
+import random
+
+arr = [random.randint(-99, 99) for _ in range(10)]
+print("old:", arr)
+
+newarr = []
+while arr:
+    elem = min(arr)
+    newarr.append(elem)
+    arr.remove(elem)
+
+print("new:", newarr)
