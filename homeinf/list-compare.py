4aeb140d1c787c7763db58f9e5894b6a133ccfc7
mykespb <mykespb@gmail.com>
2024-10-05 23:03:16 +0300

    list compare

---
+++ homeinf/list-compare.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+# Mikhail Kolodin, 2024
+# 2024-10-05 2024-10-05 1.0
+# сравнение списков
+
+import random
+
+a1 = [random.randint(0, 5) for _ in range(10)]
+a2 = [random.randint(0, 5) for _ in range(10)]
+print(f"{a1=}, {a2=}")
+
