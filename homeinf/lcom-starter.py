5a16c7940603b5a558bfca70c43d7c6bf16ed20f
mykespb <mykespb@gmail.com>
2024-10-07 15:56:47 +0300

    starter

---
+++ homeinf/lcom-starter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.10
+
+# Mikhail (myke) Kolodin, 2021
+# 2024-10-07 2024-10-07 1.0
+# lcom-starter.py
+
+# Играем со списковыми включениями
+
+import random
+
+# Получить список натуральных чисел до 10.
+
+a = [i for i in range(1, 11)]
+
+print(f"{a=}")
+
