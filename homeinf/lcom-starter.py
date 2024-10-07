7bcfae2cbd5f96066f30671666c06bb5b491478f
mykespb <mykespb@gmail.com>
2024-10-07 20:40:04 +0300

    restructure

--- homeinf/lcom-starter.py
+++ homeinf/lcom-starter.py
@@ -6,16 +6,19 @@
 
 # Играем со списковыми включениями
 
-# import random
-# Получить знакопеременный список натуральных чисел до 10.
-# Типа [1, -2, 3, -4, …]
-# a = [ (-1)**(i+1) * i for i in range(1, 11)]
+def signrow():
+    # import random
+    # Получить знакопеременный список натуральных чисел до 10.
+    # Типа [1, -2, 3, -4, …]
+    a = [ (-1)**(i+1) * i for i in range(1, 11)]
+    print(a)
 
-import string
+# signrow()
 
 def lorem():
     """lorem ipsum"""
 
+    import string
     lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     chars = string.ascii_lowercase
     lst = sorted([w.lower() for w in lorem.replace(',', '').replace('.', '').split()])
