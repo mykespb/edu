11ff39f653e83a650225443461883cace4070394
mykespb <mykespb@gmail.com>
2024-10-07 16:31:40 +0300

    lorem ipsum

--- homeinf/lcom-starter.py
+++ homeinf/lcom-starter.py
@@ -6,11 +6,18 @@
 
 # Играем со списковыми включениями
 
-import random
-
+# import random
 # Получить знакопеременный список натуральных чисел до 10.
 # Типа [1, -2, 3, -4, …]
+# a = [ (-1)**(i+1) * i for i in range(1, 11)]
 
-a = [ (-1)**(i+1) * i for i in range(1, 11)]
+# lorem ipsum
 
-print(f"{a=}")
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+import string
+chars = string.ascii_lowercase
+
+lst = sorted([w.lower() for w in lorem.replace(',', '').replace('.', '').split()])
+
+print(f"{lst}")
