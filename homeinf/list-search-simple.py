89388529daf569dd7bddda888f8833b5bcdc62fe
mykespb <mykespb@gmail.com>
2024-10-11 16:51:02 +0300

    simple search

--- homeinf/list-search-simple.py
+++ homeinf/list-search-simple.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # Mikhail Kolodin, 2024
 # list-search-simple.py
-# 2024-10-08 2024-10-08 1.0
+# 2024-10-08 2024-10-11 1.1
 # найти целое значение в списке
 
 import random
 
 a = [random.randint(0, 9) for _ in range(10)]
 
-def find(lst, val):
+def find1(lst, val):
     """ищем число val в списке lst.
     возвращает True/ Fasle"""
     
@@ -17,9 +17,9 @@ def find(lst, val):
     return val in lst
 
 
-print(f"{a=}, 0 => {find(a, 0)}")
-print(f"{a=}, -1 => {find(a, -1)}")
-print(f"{a=}, 1 => {find(a, 1)}")
+print(f"{a=}, 0 => {find1(a, 0)}")
+print(f"{a=}, -1 => {find1(a, -1)}")
+print(f"{a=}, 1 => {find1(a, 1)}")
 
 # ~ a=[0, 3, 4, 8, 0, 2, 1, 6, 2, 2], 0 => True
 # ~ a=[0, 3, 4, 8, 0, 2, 1, 6, 2, 2], -1 => False
