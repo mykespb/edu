1583108402cb1321d79799153586e197c128d7da
mykespb <mykespb@gmail.com>
2024-10-29 14:43:00 +0300

    vsra swapper

--- homeinf/simple-math.py
+++ homeinf/simple-math.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Mikhail Kolodin, 2024
 # simple-matrix.py
-# 2024-10-15 2024-10-22 3.3
+# 2024-10-15 2024-10-29 4.0
 # простые задачи по математике
 
 import random
@@ -9,17 +9,37 @@ from pprint import pp
 import copy
 import math
 
-# 0. Печать матрицы
+ri = random.randint
 
+# ============================================================================
+# 0. Создание и печать матрицы
+# ============================================================================
+
+def make1dim(elems=3, bottom=0, top=9):
+    """создать вектор"""
+    
+    return [ random.randint(bottom, top) for c in range(elems) ] 
+
+    
+def make2dim(rows=3, cols=3, bottom=0, top=9):
+    """создать матрицу"""
+    
+    return [ [ random.randint(bottom, top) for c in range(cols) ] for r in range(rows) ]
+
+    
 def mprint(a, width=4):
+    """напечатать матрицу"""
+    
     for i in range(len(a)):
         for j in range(len(a[i])):
             print(f"%{width}d" % a[i][j], end="")
         print()
-
 # ~ a = [random.randint(-99, 99) for _ in range(10)]
 
+
+# ============================================================================
 # ~ 1. Нахождение НОД по Евклиду
+# ============================================================================
 
 def gcd1(a, b):
     """find GCD of a and b"""
@@ -53,7 +73,7 @@ def gcd_tests():
         a, b = test
         print(f"GCD of {a} and {b} is {gcd1(a,b)}, {gcd2(a,b)}, {math.gcd(a,b)}")
 
-gcd_tests()
+# ~ gcd_tests()
 
 # ~ GCD of 1 and 1 is 1, 1, 1
 # ~ GCD of 1 and 2 is 1, 1, 1
@@ -63,3 +83,27 @@ gcd_tests()
 # ~ GCD of 7 and 13 is 1, 1, 1
 # ~ GCD of 1024 and 2048 is 1024, 1024, 1024
 # ~ GCD of 6 and 4096 is 2, 2, 2
+
+# ============================================================================
+# ~ 2. Простые обмены
+# ============================================================================
+
+# ~ Поменять местами значения 2 переменных
+# ~ a -> b -> a
+
+a, b = ri(1, 9), ri(1, 9)
+print(f"{a=}, {b=}")
+
+a, b = b, a
+print(f"{a=}, {b=}")
+
+# ~ Поменять местами значения 3 переменных
+# ~ a -> b -> c-> a
+
+a, b, c = ri(1, 9), ri(1, 9), ri(1, 9)
+print(f"{a=}, {b=}, {c=}")
+
+a, b, c = c, a, b
+print(f"{a=}, {b=}, {c=}")
+
+# ============================================================================
