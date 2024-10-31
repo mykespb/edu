d5649420879f3f8b84821274ba8c3e4746d0f1c6
mykespb <mykespb@gmail.com>
2024-10-31 14:47:09 +0300

    total checker, stub

---
+++ homeinf/check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# myke 2024-10-31 2024-10-31 0.1
+# check.py
+
+проверка допустимости различных данных.
+1. даты (YYYY-MM-DD, DD.MM.YYYY)
+2. ipv4-адреса
+
+from random import randint
+
+def check_date(d):
+    ...
+
+def test_check_date(times=10):
+    ...
+
+def check_ipv4(ip):
+    ...
+
+def test_check_ipv4(times=10):
+    ...
