07327d77424c5913a0088ca2ae7823145415e971
mykespb <mykespb@gmail.com>
2024-11-04 16:07:54 +0300

    down from the hill top we go

--- homeinf/hilldown.py
+++ homeinf/hilldown.py
@@ -1,34 +1,82 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2022
-# 2024-11-04 2024-11-04 0.1
+# 2024-11-04 2024-11-04 1.1
 # hilldown.py
 
-# ~ Дан прямоугольный массив (списков списков).
+# ~ Дан квадратный массив (списков списков).
 # ~ Поставить в каждую клетку высоту над уровнем моря, начиная с 0 от краёв,
 # ~ и повышая на 1 на каждый шаг к центру.
 
-def gen(rows=5, cols=5):
+def mprint(a, width=4):
+    """напечатать матрицу"""
+    
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(f"%{width}d" % a[i][j], end="")
+        print()
+        
+
+def gen(size=5):
     """создаёт массив"""
 
-    return [ [ 0 for j in range(cols) ] for i in range(rows) ]
+    return [ [ 0 for j in range(size) ] for i in range(size) ]
 
 
-def solve1(a):
-    """решение 1"""
+def solve(a):
+    """решение"""
 
-    rows = len(a)
-    cols = len(a[0])
-    top = max(rows, cols) // 2 + 1
+    size = len(a)
+    for i in range(size // 2 + 1):
+        for j in range(size-i*2):
+            a[i][i+j] = a[size-i-1][i+j] = a[i+j][i] = a[i+j][size-i-1] = i
 
-    return top
+    return a
 
 
-def main():
+def main(size=3):
     """запуск"""
 
-    a = gen()
-    print(solve1(a))
+    print("\nПоле", size, "порядка")
+    a = gen(size)
+    mprint(solve(a))
 
 
 main()
+main(5)
+main(7)
+main(9)
+
+
+# ~ Поле 3 порядка
+   # ~ 0   0   0
+   # ~ 0   1   0
+   # ~ 0   0   0
+
+# ~ Поле 5 порядка
+   # ~ 0   0   0   0   0
+   # ~ 0   1   1   1   0
+   # ~ 0   1   2   1   0
+   # ~ 0   1   1   1   0
+   # ~ 0   0   0   0   0
+
+# ~ Поле 7 порядка
+   # ~ 0   0   0   0   0   0   0
+   # ~ 0   1   1   1   1   1   0
+   # ~ 0   1   2   2   2   1   0
+   # ~ 0   1   2   3   2   1   0
+   # ~ 0   1   2   2   2   1   0
+   # ~ 0   1   1   1   1   1   0
+   # ~ 0   0   0   0   0   0   0
+
+# ~ Поле 9 порядка
+   # ~ 0   0   0   0   0   0   0   0   0
+   # ~ 0   1   1   1   1   1   1   1   0
+   # ~ 0   1   2   2   2   2   2   1   0
+   # ~ 0   1   2   3   3   3   2   1   0
+   # ~ 0   1   2   3   4   3   2   1   0
+   # ~ 0   1   2   3   3   3   2   1   0
+   # ~ 0   1   2   2   2   2   2   1   0
+   # ~ 0   1   1   1   1   1   1   1   0
+   # ~ 0   0   0   0   0   0   0   0   0
+
