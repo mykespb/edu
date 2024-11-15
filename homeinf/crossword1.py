f269e8d6195410ff6bfa762e6ed403a7b83b6a50
mykespb <mykespb@gmail.com>
2024-11-15 17:19:58 +0300

    init crossword1

---
+++ homeinf/crossword1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2022
+# 2024-11-15 2024-11-5 0.1
+# crossword1.py
+
+# Даны 2 слова. Построить кроссворд из этих 2 слов,
+# т.е. вписать их в пересечённом виде на 2-мерной таблице.
+
+vocab = 'alfa bravo charlie delta echo foxtrot golf hotel india juliett kilo lima mike november oscar' \
+    'papa quebec romeo sierra tango uniform victor whiskey xray yankee zulu'
+
+print(vocab)
