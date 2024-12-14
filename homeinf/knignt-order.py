e9efb1843b2c5ac31dc9a890dfdb589d513d9ebd
mykespb <mykespb@gmail.com>
2024-12-14 17:53:35 +0300

    символически отметить

--- homeinf/knignt-order.py
+++ homeinf/knignt-order.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2024
-# 2024-12-14 2024-12-14 1.1
+# 2024-12-14 2024-12-14 1.2
 # knight-order.py
 # На шахматной доске стоит конь (по умолчанию - на А1 = [0, 0]). Это позиция 0.
 # Отметить числами клетки доски в соответствии с тем, на каком минимальном ходу конь может оказаться на соотв. клетке.
 
 SIZE = 8
-
+EMPTY = -1
 
 def pp():
     """печать доски"""
     
-    for i in range(SIZE):
+    for i in range(SIZE-1, -1, -1):
         print()
         for j in range(SIZE):
             print(f"{board[i][j]:3d}", end="")
@@ -25,7 +25,7 @@ def mark(i, j):
     
     global board, marked, move
     
-    if 0 <= i < SIZE and 0 <= j < SIZE and board[i][j] == -1:
+    if 0 <= i < SIZE and 0 <= j < SIZE and board[i][j] == EMPTY:
         board[i][j] = move + 1
         marked += 1
         
