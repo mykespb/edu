1f6c680b3eb8679790afec27d04ea160d8d5b9ad
mykespb <mykespb@gmail.com>
2024-12-31 10:35:44 +0300

    problems list and password prompter start

--- homeinf/pass-prompter.py
+++ homeinf/pass-prompter.py
@@ -5,3 +5,36 @@
 # pass-prompter.py
 
 # ~ Подсказки для создания паролей.
+
+# ~ Многие с трудом придумывают пароли.
+# ~ Поможем им.
+# ~ Пусть пароли все будут такими:
+# ~ cvcvdddd либо vcvcdddd
+# ~ где v = volwel, c = consonant, d = digit (не 0),
+# ~ все латинские, регистр произвольный.
+
+from random import choice, random
+
+vowels     = "aeiouyAEIOUY"
+consonants = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
+digits     = "123456789"
+
+def make_pass():
+    """сделать пароль"""
+
+    out = ( ( choice(vowels) + choice(consonants) + choice(vowels) + choice(consonants)
+        if random() < .5
+        else choice(consonants) + choice(vowels) + choice(consonants) + choice(vowels) ) +
+        choice(digits) + choice(digits) + choice(digits) + choice(digits) )
+
+    return out
+
+
+def test_make_pass(times=1):
+    """тестируем пароль"""
+
+    for rept in range(times):
+        print(make_pass())
+
+
+test_make_pass(20)
