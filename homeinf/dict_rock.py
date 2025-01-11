dd9e66f9a64be39f8d5d48501ebc096b55f8b6fb
mykespb <mykespb@gmail.com>
2025-01-11 14:56:08 +0300

    оформление, errata

--- homeinf/dict_rock.py
+++ homeinf/dict_rock.py
@@ -9,30 +9,49 @@ from dict_rock_data import *
 from pprint import pp
 
 # 1. просто распечатаем всё поальбомно
+
 pp(coj)
 
 # 2. сколько песен написано и издано за первые 3 года
+
 year_start = min(x["year"] for x in coj)
+
 print("начал издавать в", year_start, "году")
+
 songs3 = []
+
 for songs in [x["songs"] for x in coj if x["year"] < year_start+3]:
     songs3 += songs
+
 print("\n\nпесни за 3 года:", songs3)
+
 print("\nза 3 года издано песен", len(songs3))
 
 # 3. а без повторов?
+
 bp = sorted(set(songs3))
+
 print("\n\nпесни за 3 года без повторов:", bp)
+
 print("\nза 3 года издано песен без повторов:", len(bp))
 
 # 4. полный алфавитный список песен
+
 songs_all = []
+
 for songs in [x["songs"] for x in coj]:
     songs_all += songs
+
 salf = []
+
 for song in songs_all:
     if song not in salf:
         salf.append(song)
+
 salf.sort()
+
 print("\n\nвсего песен:", len(salf))
+
 print("всё по алфавиту:\n", salf)
+
+# ------------------------ the end.
