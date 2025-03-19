ce39f6b51d1070d484c1000f2b0749cc234da5e4
mykespb <mykespb@gmail.com>
2025-03-19 22:29:03 +0300

    bd

--- homeinf/sql-simple.py
+++ homeinf/sql-simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Mikhail (myke) Kolodin
-# 2025-02-28 2025-03-19 1.9
+# 2025-02-28 2025-03-19 1.12
 # sql-simple.py
 
 # ~ Simple work with sqlite3 database in memory.
@@ -33,24 +33,28 @@ today = cur.fetchall()[0][0]
 print(f"{today=}")
 
 # create 1 table
-print('\nCreate') 
+print('\n=== Create ===') 
 cmd = "CREATE TABLE people (first, last, bd)"
 #cmd = "CREATE TABLE IF NOT EXISTS people (first, last, bd)"
 #cmd = "CREATE TABLE IF NOT EXISTS people (first TEXT, last TEXT, bd TEXT)"
 conn.execute(cmd)
 conn.commit()
 
+print('\nOk')
+
 # populate it with data
 
-print('\nPopulate')
+print('\n=== Populate ===')
 cur.execute("INSERT INTO people (first, last, bd) VALUES (?, ?, ?)",
     ("Some", "Body", "1999-12-31"))
 conn.commit()
 
+print('\nOk')
+
 # read all data and print them
 
-print('\nRead') 
-cmd = "SELECT * FROM people"
+print('\n=== Read ===') 
+cmd = "SELECT rowid, * FROM people"
 data = cur.execute(cmd)
 
 print('\nAll data:')
@@ -71,7 +75,7 @@ conn.commit()
 
 # read all data and print them
 
-cmd = "SELECT * FROM people"
+cmd = "SELECT rowid, * FROM people"
 data = cur.execute(cmd)
 
 print('\nAll data:')
@@ -80,22 +84,44 @@ for row in data:
     
 # read selected data    
 
-data = cur.execute("SELECT bd, last FROM people WHERE bd > '2009' ORDER BY bd DESC ")
+data = cur.execute("SELECT rowid, bd, last FROM people WHERE bd > '2009' ORDER BY bd DESC ")
 
+# ~ print(dir(data))
 # print them
-print('\nSelected data with descending birthdays:')
+print(f'\nSelected data with descending birthdays:')
+num = 0
 for row in data:
     print(row)
+    num += 1
+print(f"Found {num} record(s).")
+
+# read selected data, who has birthday this month
+
+cur.execute("SELECT date()")
+today = cur.fetchone()[0]
+month = today[5:7]
+
+print(f"\nLook for persons of current {month=}")
+
+data = cur.execute("SELECT rowid, * FROM people WHERE substr(bd, 6, 2) = ? ORDER BY bd ASC ", (month,))
+
+# print them
+print('\nSelected data with ascending birthdays:')
+num = 0
+for row in data:
+    print(row)
+    num += 1
+print(f"Found {num} record(s).")
 
 # update db
 
-print('\nUpdate') 
+print('\n=== Update ===') 
 cur.execute("UPDATE people SET bd = '2000-01-01' WHERE last = 'Soft' ")
 conn.commit()
 
 # read all data and print them
 
-cmd = "SELECT * FROM people"
+cmd = "SELECT rowid, * FROM people"
 data = cur.execute(cmd)
 print('\nAll data:')
 for row in data:
@@ -103,13 +129,13 @@ for row in data:
 
 # delete data
 
-print('\nDelete') 
-cur.execute("DELETE FROM people WHERE last = 'Sooner' ")
+print('\n=== Delete ===') 
+cur.execute("DELETE FROM people WHERE last = 'Done' ")
 conn.commit()
 
 # read all data and print them
 
-cmd = "SELECT * FROM people"
+cmd = "SELECT rowid, * FROM people"
 data = cur.execute(cmd)
 print('\nAll data:')
 for row in data:
@@ -131,43 +157,54 @@ print("\nDB is closed. No reading or writing is possible now.\nGood-bye!\n")
 # ~ today='2025-03-19'
 # ~ today='2025-03-19'
 
-# ~ Create
+# ~ === Create ===
 
-# ~ Populate
+# ~ Ok
 
-# ~ Read
+# ~ === Populate ===
 
-# ~ All data:
-# ~ ('Some', 'Body', '1999-12-31')
+# ~ Ok
 
-# ~ All data:
-# ~ ('Some', 'Body', '1999-12-31')
-# ~ ('John', 'Done', '2010-01-21')
-# ~ ('Mike', 'Soft', '2012-02-11')
-# ~ ('Mike', 'Hard', '2015-03-01')
-# ~ ('Dennis', 'Sooner', '2008-05-31')
-
-# ~ Selected data:
-# ~ ('2015-03-01', 'Hard')
-# ~ ('2012-02-11', 'Soft')
-# ~ ('2010-01-21', 'Done')
-
-# ~ Update
+# ~ === Read ===
 
 # ~ All data:
-# ~ ('Some', 'Body', '1999-12-31')
-# ~ ('John', 'Done', '2010-01-21')
-# ~ ('Mike', 'Soft', '2000-01-01')
-# ~ ('Mike', 'Hard', '2015-03-01')
-# ~ ('Dennis', 'Sooner', '2008-05-31')
-
-# ~ Delete
+# ~ (1, 'Some', 'Body', '1999-12-31')
 
 # ~ All data:
-# ~ ('Some', 'Body', '1999-12-31')
-# ~ ('John', 'Done', '2010-01-21')
-# ~ ('Mike', 'Soft', '2000-01-01')
-# ~ ('Mike', 'Hard', '2015-03-01')
+# ~ (1, 'Some', 'Body', '1999-12-31')
+# ~ (2, 'John', 'Done', '2010-01-21')
+# ~ (3, 'Mike', 'Soft', '2012-02-11')
+# ~ (4, 'Mike', 'Hard', '2015-03-01')
+# ~ (5, 'Dennis', 'Sooner', '2008-05-31')
+
+# ~ Selected data with descending birthdays:
+# ~ (4, '2015-03-01', 'Hard')
+# ~ (3, '2012-02-11', 'Soft')
+# ~ (2, '2010-01-21', 'Done')
+# ~ Found 3 record(s).
+
+# ~ Look for persons of current month='03'
+
+# ~ Selected data with ascending birthdays:
+# ~ (4, 'Mike', 'Hard', '2015-03-01')
+# ~ Found 1 record(s).
+
+# ~ === Update ===
+
+# ~ All data:
+# ~ (1, 'Some', 'Body', '1999-12-31')
+# ~ (2, 'John', 'Done', '2010-01-21')
+# ~ (3, 'Mike', 'Soft', '2000-01-01')
+# ~ (4, 'Mike', 'Hard', '2015-03-01')
+# ~ (5, 'Dennis', 'Sooner', '2008-05-31')
+
+# ~ === Delete ===
+
+# ~ All data:
+# ~ (1, 'Some', 'Body', '1999-12-31')
+# ~ (3, 'Mike', 'Soft', '2000-01-01')
+# ~ (4, 'Mike', 'Hard', '2015-03-01')
+# ~ (5, 'Dennis', 'Sooner', '2008-05-31')
 
 # ~ DB is closed. No reading or writing is possible now.
 # ~ Good-bye!
