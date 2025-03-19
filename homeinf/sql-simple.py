51449656f256a063b58668f6c4775fb7e1326b37
mykespb <mykespb@gmail.com>
2025-03-19 16:57:31 +0300

    same

--- homeinf/sql-simple.py
+++ homeinf/sql-simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Mikhail (myke) Kolodin
-# 2025-02-28 2025-03-19 1.6
+# 2025-02-28 2025-03-19 1.9
 # sql-simple.py
 
 # ~ Simple work with sqlite3 database in memory.
@@ -11,11 +11,17 @@ import sqlite3 as sql
 #db = '/tmp/one.db'
 db = ":memory:"
 
-print("Hello! Let's play with database.")
+print("\nHello! Let's play with database.\n")
 
 # make connection
 conn = sql.connect(db)
 cur = conn.cursor()
+
+# make calcuilations
+
+cur.execute("SELECT 2*2")
+result = cur.fetchone()[0]
+print(f"{result=}")
 
 # get current date
 cur.execute("SELECT date()")
@@ -28,13 +34,31 @@ print(f"{today=}")
 
 # create 1 table
 print('\nCreate') 
-cmd = "CREATE TABLE IF NOT EXISTS people (first TEXT, last TEXT, bd TEXT)"
+cmd = "CREATE TABLE people (first, last, bd)"
+#cmd = "CREATE TABLE IF NOT EXISTS people (first, last, bd)"
+#cmd = "CREATE TABLE IF NOT EXISTS people (first TEXT, last TEXT, bd TEXT)"
 conn.execute(cmd)
 conn.commit()
 
 # populate it with data
 
-print('\nPopulate') 
+print('\nPopulate')
+cur.execute("INSERT INTO people (first, last, bd) VALUES (?, ?, ?)",
+    ("Some", "Body", "1999-12-31"))
+conn.commit()
+
+# read all data and print them
+
+print('\nRead') 
+cmd = "SELECT * FROM people"
+data = cur.execute(cmd)
+
+print('\nAll data:')
+for row in data:
+    print(row)
+
+# populate many
+
 persons = [
     ("John", "Done", "2010-01-21"),
     ("Mike", "Soft", "2012-02-11"),
@@ -45,12 +69,11 @@ persons = [
 cur.executemany("INSERT INTO people (first, last, bd) VALUES (?, ?, ?)", persons)
 conn.commit()
 
-# read all data
-print('\nRead') 
+# read all data and print them
+
 cmd = "SELECT * FROM people"
 data = cur.execute(cmd)
 
-# print them
 print('\nAll data:')
 for row in data:
     print(row)
@@ -60,7 +83,7 @@ for row in data:
 data = cur.execute("SELECT bd, last FROM people WHERE bd > '2009' ORDER BY bd DESC ")
 
 # print them
-print('\nSelected data:')
+print('\nSelected data with descending birthdays:')
 for row in data:
     print(row)
 
@@ -70,7 +93,8 @@ print('\nUpdate')
 cur.execute("UPDATE people SET bd = '2000-01-01' WHERE last = 'Soft' ")
 conn.commit()
 
-# read all data
+# read all data and print them
+
 cmd = "SELECT * FROM people"
 data = cur.execute(cmd)
 print('\nAll data:')
@@ -83,7 +107,8 @@ print('\nDelete')
 cur.execute("DELETE FROM people WHERE last = 'Sooner' ")
 conn.commit()
 
-# read all data
+# read all data and print them
+
 cmd = "SELECT * FROM people"
 data = cur.execute(cmd)
 print('\nAll data:')
@@ -101,7 +126,10 @@ print("\nDB is closed. No reading or writing is possible now.\nGood-bye!\n")
 # --- results: ---
 
 # ~ Hello! Let's play with database.
-# ~ today='2025-03-17'
+
+# ~ result=4
+# ~ today='2025-03-19'
+# ~ today='2025-03-19'
 
 # ~ Create
 
@@ -110,26 +138,36 @@ print("\nDB is closed. No reading or writing is possible now.\nGood-bye!\n")
 # ~ Read
 
 # ~ All data:
+# ~ ('Some', 'Body', '1999-12-31')
+
+# ~ All data:
+# ~ ('Some', 'Body', '1999-12-31')
 # ~ ('John', 'Done', '2010-01-21')
 # ~ ('Mike', 'Soft', '2012-02-11')
+# ~ ('Mike', 'Hard', '2015-03-01')
 # ~ ('Dennis', 'Sooner', '2008-05-31')
 
 # ~ Selected data:
+# ~ ('2015-03-01', 'Hard')
 # ~ ('2012-02-11', 'Soft')
 # ~ ('2010-01-21', 'Done')
 
 # ~ Update
 
 # ~ All data:
+# ~ ('Some', 'Body', '1999-12-31')
 # ~ ('John', 'Done', '2010-01-21')
 # ~ ('Mike', 'Soft', '2000-01-01')
+# ~ ('Mike', 'Hard', '2015-03-01')
 # ~ ('Dennis', 'Sooner', '2008-05-31')
 
 # ~ Delete
 
 # ~ All data:
+# ~ ('Some', 'Body', '1999-12-31')
 # ~ ('John', 'Done', '2010-01-21')
 # ~ ('Mike', 'Soft', '2000-01-01')
+# ~ ('Mike', 'Hard', '2015-03-01')
 
 # ~ DB is closed. No reading or writing is possible now.
 # ~ Good-bye!
