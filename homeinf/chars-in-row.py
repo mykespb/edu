3f983d8b44ef082a85ffb42b0ea8988944ad5085
mykespb <mykespb@gmail.com>
2025-04-03 20:51:08 +0300

    chars in a row

--- homeinf/chars-in-row.py
+++ homeinf/chars-in-row.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Mikhail (myke) Kolodin
-# 2025-04-03 2025-04-03 1.1
+# 2025-04-03 2025-04-03 1.2
 # chars-in-rows.py
 
 # ~ Даётся массив из строк, необходимо вернуть те строки из массива, которые могут быть набраны лишь при использовании знаков из одного ряда клавиатуры.
@@ -13,25 +13,39 @@ kbd = "QWERTYUIOP ASDFGHJKL ZXCVBNM" .split()
 
 
 # tests:
+
 words1 = ["Hello", "Alaska", "Dad", "Peace"]
 words2 = ["omg", 'gosh', 'devil', 'hell']
+
+
+# solution:
+
+skbd = list(map(set, kbd))
 
 
 def check(words):
     """check all words"""
     
-    out = []
-    for word in words:
-        if good(word):
-            out.append(word)
+    # ~ goods = []
+    # ~ for word in words:
+        # ~ if good(word):
+            # ~ goods.append(word)
 
-    print(f"{words} => {out}")
+    goods = [ word for word in words if good(word) ] 
+
+    print(f"{words} => {goods}")
 
 
 def good(word):
     """check one word"""
 
-    skbd = list(map(set, kbd))
+    # ~ sword = set(word.upper())
+
+    # ~ for row in skbd:
+        # ~ if sword <= row:
+            # ~ return True
+
+    # ~ return False
 
     return any( [set(word.upper()) <= row for row in skbd] )
     
