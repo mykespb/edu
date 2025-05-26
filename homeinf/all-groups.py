fe831ce5eaa16414fe8c81d3f281781011efa071
mykespb <mykespb@gmail.com>
2025-05-26 11:02:11 +0300

    better tests

--- homeinf/all-groups.py
+++ homeinf/all-groups.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-# Mikhail (myke) Kolodin, 2021
-# 2025-05-23 2025-05-25 1.3
+# Mikhail (myke) Kolodin, 2025
+# 2025-05-23 2025-05-26 1.4
 
 # ~ Во всех кружках
 
-# ~ Есть данные о школьникоах и в каких кружках они участвуют.
+# ~ Есть данные о школьниках и в каких кружках они участвуют.
 # ~ Выписать тех, кто участвует сразу во всех кружках,
 # ~ если такие школьники есть.
 
 from pprint import pprint
 
 pups = {
-    'Cate': 'swimming dancing drawing' . split(),
+    'Kate': 'swimming dancing drawing drawing drawing drawing drawing drawing drawing' . split(),
     'Jane': 'swimming running' . split(),
     'Nick': 'drawing running' . split(),
     'Pete': 'dancing math chemistry swimming drawing running' . split(),
@@ -21,7 +21,7 @@ pups = {
     # ~ 'Joker': 'go went gone' . split(),
     }
 
-# ~ pprint(pups)
+pprint(pups)
 
 # ~ kru = set()
 # ~ for v in pups.values():
@@ -33,7 +33,8 @@ krulen = len( kru := set() . union (*map(set, pups.values() ) ) )
 # ~ krulen = len(kru)
 # ~ print(krulen, kru)   
 
-print("most busy pups:", 
-    ", " .join ( [k for k in pups if len(pups[k]) == krulen] )
+print("Most busy pups:", 
+    ", " .join ( [k for k in pups if set(pups[k]) == kru] )
+    # ~ ", " .join ( [k for k in pups if len(set(pups[k])) == krulen] )
     or "No such pup"
     )
