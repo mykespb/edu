c31770b03b86364de6ed24456e9424cb2a4685b9
mykespb <mykespb@gmail.com>
2025-05-30 14:07:38 +0300

    good bargain

--- homeinf/whowhat.py
+++ homeinf/whowhat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2025
-# 2025-05-30 2025-05-30 2.0
+# 2025-05-30 2025-05-30 2.1
 # ~ whowhat.py 
 # ~ Кто что купил?
 # ~ Есть список сделок, причём указаны как покупатели, так и покупки, вещи, деньги.
@@ -25,7 +25,7 @@ def gen(size=10):
         thing     = choice(things)
         price     = randint(100, 100_000)
         tax       = randint(1, price // 5)
-        lot.append( (pers_from, pers_to, thing, tax) )
+        lot.append( (pers_from, pers_to, thing, price-tax) )
         lot.append( (pers_to, pers_from, thing, -price) )
 
     shuffle(lot)
@@ -43,7 +43,7 @@ def solve(lot):
             for second in lot:
                 if second[:3] == (pers_to, pers_from, thing):
                     price = -second[-1]
-                    print(f"{pers_from} sold {thing} to {pers_to} for ${price:_} with tax ${tax:_}")
+                    print(f"{pers_from} sold {thing} to {pers_to} for ${price:_} with tax {(price - tax) * 100 / price :.2f}% as ${price - tax}")
 
     
 
@@ -57,13 +57,13 @@ def main():
 main()
 
 # ~ result:
-# ~ Zoe sold chair to Isla for $24_480 with tax $4_346
-# ~ Rachel sold car to Mila for $6_302 with tax $170
-# ~ Uma sold dog to Jake for $80_560 with tax $2_678
-# ~ Michael sold chair to Alla for $92_838 with tax $3_553
-# ~ Boris sold boat to Laura for $56_735 with tax $5_182
-# ~ Isla sold goat to Lily for $95_572 with tax $14_024
-# ~ Reece sold phone to Helen for $58_276 with tax $1_898
-# ~ Charlie sold jewel to Jacob for $24_015 with tax $1_697
-# ~ Helen sold voyage to Jake for $87_846 with tax $14_020
-# ~ Heather sold lamp to Lily for $68_473 with tax $1_847
+# ~ William sold crown to Nina for $37_719 with tax 8.52% as $3215
+# ~ Victoria sold lamp to Michael for $76_265 with tax 10.54% as $8035
+# ~ Jane sold crown to Mason for $40_507 with tax 13.50% as $5469
+# ~ Charlotte sold flower to Milana for $83_070 with tax 0.77% as $641
+# ~ Milana sold lamp to Pamela for $8_579 with tax 16.76% as $1438
+# ~ David sold flower to Nina for $54_621 with tax 2.90% as $1583
+# ~ Sonya sold flower to Victoria for $36_571 with tax 12.23% as $4474
+# ~ Reece sold computer to Jane for $82_011 with tax 17.61% as $14446
+# ~ Pamela sold cat to Emily for $72_681 with tax 16.56% as $12039
+# ~ Alex sold boat to David for $73_470 with tax 19.68% as $14458
