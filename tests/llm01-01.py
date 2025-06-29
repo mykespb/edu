362d72af0367a7221647ac2e9ad08bb4f266ea95
mykespb <mykespb@gmail.com>
2025-06-29 23:24:36 +0300

    ai version of sorting

---
+++ tests/llm01-01.py
@@ -0,0 +1,20 @@
+def create_test_data():
+    test_list = [5, 2, 8, 1, 9]
+    return test_list
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+def display_result(test_data, sorted_data):
+    print("Initial Test Data:", test_data)
+    print("Sorted Result:", sorted_data)
+
+test_data = create_test_data()
+sorted_data = quicksort(test_data)
+display_result(test_data, sorted_data)
