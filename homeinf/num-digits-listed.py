ceb00c6153faeb08f3021ccdacd1693e435a7961
mykespb <mykespb@gmail.com>
2025-08-24 21:21:36 +0300

    quo + num of num

--- homeinf/num-digits-listed.py
+++ homeinf/num-digits-listed.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2025
-# 2025-08-24 2025-008-24 1.0
+# 2025-08-24 2025-08-24 1.0
 # ~ num-digits-listed.py
 
 # ~ Числа с цифрами
@@ -10,10 +10,12 @@
 # ~ Дан набор (случайных) чисел.
 # ~ Вывести:
 # ~ сколько чисел начинаются на 1, сколько на 2, и т.д., сколько нулей.
-# ~ М.б. и показать (отсортированные) списки таких чисел по группам по 1 цифре.
+# ~ и показать (отсортированные любым способом) списки таких чисел по группам по 1 цифре.
+
 
 import random
 from collections import defaultdict
+
 
 def make(num = 10):
     """make numbers"""
@@ -34,7 +36,8 @@ def solve(nums):
 
     print("result:")
     for k in sorted(cnt.keys()):
-        print(f"1st digit: {k}, number of numbers: {cnt[k]}")
+        print(f"1st digit: {k}, number of numbers: {len(cnt[k])}, numbers: ", end="")
+        print(*(sorted(cnt[k])), sep=", ")
 
 
 def main():
@@ -48,12 +51,11 @@ main()
 
 
 # ~ numbers:
-# ~ 161, 507, 427, 391, 285, 6, 329, 999, 103, 649
+# ~ 125, 897, 845, 582, 597, 952, 701, 660, 851, 77
 # ~ result:
-# ~ 1st digit: 1, number of numbers: 2
-# ~ 1st digit: 2, number of numbers: 1
-# ~ 1st digit: 3, number of numbers: 2
-# ~ 1st digit: 4, number of numbers: 1
-# ~ 1st digit: 5, number of numbers: 1
-# ~ 1st digit: 6, number of numbers: 2
-# ~ 1st digit: 9, number of numbers: 1
+# ~ 1st digit: 1, number of numbers: 1, numbers: 125
+# ~ 1st digit: 5, number of numbers: 2, numbers: 582, 597
+# ~ 1st digit: 6, number of numbers: 1, numbers: 660
+# ~ 1st digit: 7, number of numbers: 2, numbers: 701, 77
+# ~ 1st digit: 8, number of numbers: 3, numbers: 897, 845, 851
+# ~ 1st digit: 9, number of numbers: 1, numbers: 952
