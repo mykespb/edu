e08b1a6a1dd9bd4ead720900f1b3d9b3d93ed7ec
mykespb <mykespb@gmail.com>
2025-09-24 22:57:19 +0300

    re test start

--- homeinf/re-test.py
+++ homeinf/re-test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2025
-# 2025-09-24 2025-09-24 0.1
+# 2025-09-24 2025-09-24 1.0
 # re-test.py
 
 import re
@@ -16,18 +16,7 @@ print("""------------------------------------------
 example 1
 ------------------------------------------""")
 
-txt = "The rain in Spain"
-x = re.search("^The.*Spain$", txt)
-
-print(f"{x=}, [{x}]")
-if x:
-  print("YES! We have a match!")
-else:
-  print("No match")
-
-print("""------------------------------------------
-example 2
-------------------------------------------""")
+# ~ fid simple text
 
 txt = """
 How many roads must a man walk down
@@ -49,8 +38,10 @@ for i, ex in enumerate(re.findall(r'how', txt)):
     print(i, ex)
 
 print("""------------------------------------------
-example 3
+example 2
 ------------------------------------------""")
+
+# ~ find or split by simple pattern
 
 txt = "The rain in Spain"
 x = re.search(r"\s", txt)
@@ -58,18 +49,35 @@ x = re.search(r"\s", txt)
 print("The first white-space character is located in position:", x.start()) 
 
 print("""------------------------------------------
-example 4
+example 3
 ------------------------------------------""")
 
-#Split the string at every white-space character:
+# split the string at every white-space character:
 
 txt = "The rain in Spain"
 x = re.split(r"\s", txt)
 print(x)
 
 print("""------------------------------------------
+example 4
+------------------------------------------""")
+
+# ~ find by more complex pattern
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+print(f"{x=}, [{x}]")
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
+print("""------------------------------------------
 example 5
 ------------------------------------------""")
+
+# ~ match all group
 
 m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist")
 print(m.group(0))
@@ -81,6 +89,8 @@ print("""------------------------------------------
 example 6
 ------------------------------------------""")
 
+# ~ match groups and vars
+
 m = re.match(r"(\d+)\.(\d+)", "24.1632")
 print(m.groups())
 
@@ -88,6 +98,30 @@ m = re.match(r"(\d+)\.(\d+)\.(\d+)", "20.03.1968")
 print(m.groups())
 day, month, year = map(int, m.groups())
 print(f"{year=}, {month=}, {day=}")
+
+print("""------------------------------------------
+example 7
+------------------------------------------""")
+
+# ~ substitute texts by simple templates
+
+announce1 = 'Имеется прекрасное шило!'
+print(f"{announce1=}")
+announce2 = re.sub(r'шило', 'мыло', announce1)
+print(f"{announce2=}")
+
+print("""------------------------------------------
+example 8
+------------------------------------------""")
+
+# ~ substitute texts by more complex templates
+
+announce1 = 'Имеется прекрасное шило! Если бы пило, то не выло б. На два кило ещё! Два Ужасных Киловатта!'
+print(f"{announce1=}")
+announce2 = re.sub(r'([кпш]ило)', r'мыло', announce1)
+print(f"{announce2=}")
+announce3 = re.sub(r'([кпш]ило)', r'мыло (а было слово: \1)', announce1, flags=re.IGNORECASE)
+print(f"{announce3=}")
 
 print("""------------------------------------------
 the end.
