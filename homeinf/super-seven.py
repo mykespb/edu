41cef2269a8e380f7a0320215a3ac179178facd8
mykespb <mykespb@gmail.com>
2025-10-19 20:19:37 +0300

    super seven ok

---
+++ homeinf/super-seven.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2025
+# 2025-10-19 2025-10-19 1.0
+# super-seven.py
+
+# ~ Есть список чисел.
+# ~ Напечатать максимальное, в состав которого входит цифра 7.
+
+from random import randint
+
+MIN, MAX = 10, 99
+#MIN, MAX = 100_000, 999_999
+# ~ MIN, MAX = 100_000_000, 999_999_999
+
+
+def make():
+    """make list"""
+
+    return [ randint(MIN, MAX) for _ in range(10) ]
+
+
+def main():
+    """run it"""
+
+    m = make()    # normal test
+    # ~ m = [1]   # spec bad test
+    print("list:", m)
+
+    sub7 = [n for n in m if "7" in str(n)]
+    if sub7:
+        print("best:", max(sub7))
+    else:
+        print("alas, no 7s")
+
+
+main()
+
+
+
