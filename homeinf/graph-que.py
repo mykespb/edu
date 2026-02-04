419c654ef2f6d3c00c48678247e02013d7c86ed7
mykespb <mykespb@gmail.com>
2026-02-04 11:59:12 +0300

    debugs

--- homeinf/graph-que.py
+++ homeinf/graph-que.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2025
-# 2025-11-21 2026-02-04 2.1
+# 2025-11-21 2026-02-04 2.2
 # graph-que.py
 
 # обход графа, бинарное дерево, обход в ширину
 
 from pprint import pp
 
-DEBUG = 0
-# ~ DEBUG = 1
+# ~ DEBUG = 0
+DEBUG = 1
 
 sg = [1,
         [2,
@@ -33,19 +33,27 @@ pp(sg)
 def fn(g, n):
     """найти число n в графе g"""
 
-    if (DEBUG): print(f"{n=}, {g=}")
+    if (DEBUG):
+        itern = 0
+        print(f"\n{n=}, {g=}")
+        
     if g[0] == n:
         return True
 
     que = g[1:]
     
     while que:
-        if (DEBUG): print(f"iteration: {que=}")
+        if (DEBUG):
+            itern += 1
+            print(f"iteration {itern}: \n{que=}")
 
         nn = que.pop(0)
-        if (DEBUG): print(f"{nn=}")
+        if (DEBUG):
+            print(f"{nn=}")
+            print(f"{que=}")
 
-        if type(nn) == list:
+        # ~ if type(nn) == list:
+        if nn:
             if nn[0] == n:
                 return True
             que.extend(nn[1:])
