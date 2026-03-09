08da681dc35dc48f00b35a28507b15ff3a310509
mykespb <mykespb@gmail.com>
2026-03-10 00:11:10 +0300

    classes persons getters setters

---
+++ tests/classes/mainer.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# Miklhail (myke) Kolodin
+# classes / mainer.py - testing classes
+# 2026-03-09 2026-03-10 1.2
+
+from datetime import date
+
+
+class Person:
+    def __init__(self, name = "Noname", bd = date.today(), sex = True):
+        self.name = name
+        self.sex = sex
+        if bd <= date.today():
+            self.bd = bd
+        else:
+            self.bd = date.today()
+            raise ValueError
+
+    @property
+    def age(self):
+        return date.today().year - self.bd.year
+
+    @property
+    def birth(self):
+        return self.bd
+
+    @birth.setter
+    def birth(self, value):
+        if value > date.today():
+            raise ValueError
+        self.bd = value
+
+    def __str__(self):
+        return f"Person(name='{self.name}', bd='{self.bd}', sex='{self.sex}')"
+
+
+class Student(Person):
+    def __init__(self, name = "Noname", bd = date.today(), sex = True, group = None, grade = 1):
+        super().__init__(name, bd, sex)
+        self.group = group
+        self.grade = grade
+
+    def __str__(self):
+        return f"Student(name='{self.name}', bd='{self.bd}', sex='{self.sex}', group={self.group}, grade={self.grade})"
+
+
+class Teacher(Person):
+
+    MIN_SALARY = 100
+    MAX_SALARY = 1000
+    
+    def __init__(self, name = "Noname", bd = date.today(), sex = True, groups = None, salary = 0):
+        super().__init__(name, bd, sex)
+        if groups is None:
+            self.groups = []
+        else:
+            self.groups = groups
+        self.salary = salary
+
+    def add_group(self, gruppa):
+        if gruppa not in self.groups:
+            self.groups.append(gruppa)
+
+    def del_group(self, gruppa):
+        if gruppa in self.groups:
+            self.groups.remove(gruppa)
+
+    def set_salary(self, value):
+        if Teacher.MIN_SALARY <= value <= Teacher.MAX_SALARY:
+            self.salary = value
+        else:
+            raise ValueError
+
+    def __str__(self):
+        return f"Teacher(name='{self.name}', bd='{self.bd}', sex='{self.sex}', groups={self.groups}, salary={self.salary})"
+            
+
+def main():
+    print("Hello from classes!")
+
+    # Person ------------------------------------------------
+    
+    boy = Person(bd=date(2000, 3, 4))
+    print(boy)
+    boy.name = 'Юра'
+    print(boy)
+    print("age of boy:", boy.age)
+
+    girl = Person(name='Алиса', sex=False)
+    print(girl)
+
+    print("boy's birthday:", boy.birth)
+
+    try:
+        noman = Person('Некто', date(2030, 1, 1))
+        print("noman:", noman)
+    except:
+        print("Гость из будущего 2030 не принят.")
+        noman = Person('Новак', date(1999, 3, 5))
+
+    try:
+        noman.birth = date(2010, 11, 11)
+        print("noman:", noman)
+    except:
+        print("Гость из будущего 2010 не принят.")
+
+    print("Noman:", noman)
+    print("Noman's birth:", noman.birth)
+
+    # Student ------------------------------------------------
+    
+    stud1 = Student(name="Вася", group=4505)
+    print(stud1)
+
+    stud2 = Student(name="Катя", group=4705, grade=3)
+    print(stud2)
+
+    # Teacher ------------------------------------------------
+
+    prep1 = Teacher(name='Иван Петрович', bd=date(1970, 1, 1))
+    print(prep1)
+
+    prep1.add_group(123)
+    print(prep1)
+
+    prep1.add_group(123)
+    print(prep1)
+
+    prep1.add_group(230)
+    print(prep1)
+
+    prep1.set_salary(100)
+    print(prep1)
+
+    try:
+        prep1.set_salary(10000)
+        print(prep1)
+    except:
+        print("Была ошибка с зарплатой в 10000 монет, но мы справились!")
+
+    prep1.del_group(123)
+    print(prep1)
+
+    prep1.del_group(123)
+    print(prep1)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# ~ Полезно:
+# ~ https://habr.com/ru/articles/1000378/
