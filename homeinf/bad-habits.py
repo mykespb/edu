d3992ce45dd8524ee1ffcbfbcd2dbf76decd392d
mykespb <mykespb@gmail.com>
2026-03-12 20:13:00 +0300

    bad python habits

---
+++ homeinf/bad-habits.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2024
+# 2026-03-12 2026-03-12 1.0
+# bad-habits.py
+# Плохие идеи для питона
+
+from datetime import date, time
+
+# -------------------------------------------------------
+sepa = 80 * "="
+subsepa = 80 * "-"
+
+def part(name):
+    print(f"{sepa}\n{name}\n{sepa}")
+
+def subpart(name):
+    print(f"{subsepa}\n{name}\n{subsepa}")
+
+# -------------------------------------------------------
+part("Bools")
+
+subpart("Assignment")
+
+# bad
+
+year = date.today().year
+
+def fun3():
+
+    if year == 2026:
+        go = True
+    else:
+        go = False
+    print(go)
+
+    # good
+
+    go = year == 2026
+    print(go)
+
+    print(year == 2026)
+
+    subpart("Comparison")
+
+    # bad
+
+    if go == True:
+        print("True")
+    else:
+        print("False")
+
+    # good
+
+    print(go)
+
+# ~ fun3()
+
+# -------------------------------------------------------
+part("Branching")
+
+subpart("break, continue, return, exit")
+
+def fun1a():
+    if year == 2026:
+        print("This year")
+        return
+    else:
+        print("Another year")
+        return
+
+def fun1b():
+    if year == 2026:
+        print("This year")
+    else:
+        print("Another year")
+
+def fun1c():
+    print( "This year" if year == 2026 else "Another year") 
+
+# ~ fun1a()
+# ~ fun1b()
+# ~ fun1c()
+    
+# -------------------------------------------------------
+part("Print")
+
+subpart("Manual / auto formatting")
+
+def fun2a():
+    print("The answer for 2*2 is", 2*2, "and for 3*3 is", 3*3)
+
+def fun2b():
+    print(f"The answer for 2*2 is {2*2} and for 3*3 is {3*3}")
+
+fun2a()
+fun2b()
+
+# -------------------------------------------------------
+part("The End.")
+# -------------------------------------------------------
