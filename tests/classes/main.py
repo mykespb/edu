3d04457b908ab3f67657e6b36c646767254b8118
mykespb <mykespb@gmail.com>
2026-03-14 19:44:35 +0300

    type int

--- tests/classes/main.py
+++ tests/classes/main.py
@@ -7,7 +7,7 @@ from datetime import date
 
 
 class Person:
-    def __init__(self, name = "Noname", bd = date.today(), sex = True):
+    def __init__(self, name="Noname", bd=date.today(), sex=True):
         self.name = name
         self.bd = bd
         self.sex = sex
@@ -21,7 +21,7 @@ class Person:
 
 
 class Student(Person):
-    def __init__(self, name = "Noname", bd = date.today(), sex = True, group = None, grade = 1):
+    def __init__(self, name="Noname", bd=date.today(), sex=True, group=None, grade=1):
         super().__init__(name, bd, sex)
         self.group = group
         self.grade = grade
@@ -31,11 +31,10 @@ class Student(Person):
 
 
 class Teacher(Person):
+    MIN_SALARY: int = 100
+    MAX_SALARY: int = 1000
 
-    MIN_SALARY = 100
-    MAX_SALARY = 1000
-    
-    def __init__(self, name = "Noname", bd = date.today(), sex = True, groups = None, salary = 0):
+    def __init__(self, name="Noname", bd=date.today(), sex=True, groups=None, salary=0):
         super().__init__(name, bd, sex)
         if groups is None:
             self.groups = []
@@ -59,18 +58,18 @@ class Teacher(Person):
 
     def __str__(self):
         return f"Teacher(name='{self.name}', bd='{self.bd}', sex='{self.sex}', groups={self.groups}, salary={self.salary})"
-            
+
 
 def main():
     print("Hello from classes!")
 
     boy = Person(bd=date(2000, 3, 4))
     print(boy)
-    boy.name = 'Юра'
+    boy.name = "Юра"
     print(boy)
     print("age of boy:", boy.age)
 
-    girl = Person(name='Алиса', sex=False)
+    girl = Person(name="Алиса", sex=False)
     print(girl)
 
     stud1 = Student(name="Вася", group=4505)
@@ -79,7 +78,7 @@ def main():
     stud2 = Student(name="Катя", group=4705, grade=3)
     print(stud2)
 
-    prep1 = Teacher(name='Иван Петрович', bd=date(1970, 1, 1))
+    prep1 = Teacher(name="Иван Петрович", bd=date(1970, 1, 1))
     print(prep1)
 
     prep1.add_group(123)
@@ -105,7 +104,7 @@ def main():
 
     prep1.del_group(123)
     print(prep1)
-    
+
 
 if __name__ == "__main__":
     main()
