588790acca7dc2e05fb62dfee2ab41a125d3f1c5
mykespb <mykespb@gmail.com>
2026-03-22 19:56:34 +0300

    all bads

--- homeinf/all-bads.py
+++ homeinf/all-bads.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2024
-# 2026-03-22 2026-03-22 0.1
+# 2026-03-22 2026-03-22 1.0
 # all-bads.py
 # Плохие идеи для питона
 
@@ -13,7 +13,6 @@
 
 # ~ ps = sum(); print(ps)
 
-# ~ --- 
 
 # ~ - использование имени станд. функции, имя теперь в программе недоступно
 # ~ - неравномерная запись выражения
@@ -25,4 +24,97 @@
 # ~ - полезно делать описание функции в строке документирования
 # ~ - print(sum(range(10)))
 
-# ~ ---
+
+# ~ --- подсчёт нужных чисел в списке ---
+
+from random import randint
+
+# ~ заполнить списорк
+lst = []
+for i in range(100):
+    numba = randint(-999, 999)
+    lst = lst + [numba]
+
+print(lst)
+
+# ~ найти положительные
+
+ckolka = 0
+for nummer in range(100):
+    cislox = lst  [nummer]
+    if cislox == 0 or cislox < 0:
+        plus = 0
+    else:
+        ckolka = ckolka  + lst [nummer]
+
+print(ckolka)
+
+
+# ~ - i не используется;
+# ~ либо используем,
+# ~ либо _,
+# ~ либо другой способ,
+# ~ напр., list comprehension:
+    # ~ RANGE = 999
+    # ~ LENGTH = 100
+    # ~ lst = [randint(_RANGE, +RANGE) for _ in range(LENGTH)]
+# ~ - не нужна отдельнная переменная numba
+# ~ и её имя похоже имя станд. библиотеки
+# ~ и лучше append
+
+# ~ - что считаем??? количество чисел, их сумму или формируем список положительных???
+# ~ - плохие имена переменных
+# ~ or не нужен, лучше <=
+# ~ а ещё лучше оставить только истинную часть
+# ~ - лишняя переменная plus
+# ~ - избыточная повторная выборка ckolka в 48
+
+
+# ~ --- найти общие числа двух списков ---
+
+lst1 = lst2 = []
+
+for i in range(10):
+    lst1.append(randint(1, 9))
+
+for i in range(10):
+    lst2.append(randint(1, 9))
+
+lst_rezultiruyuschij = []
+for pervy in range(10):
+    for vtaroi in range(10):
+        if lst1[pervy] == lst2[vtaroi]:
+            lst_rezultiruyuschij.append(lst1[pervy])
+
+print(lst1)
+print(lst2)
+print(lst_rezultiruyuschij)
+
+
+# ~ работает неправильно
+
+# ~ надо(1)
+
+lst1 = [randint(1, 9) for _ in range(10)]
+lst2 = [randint(1, 9) for _ in range(10)]
+lst_res = []
+
+for e in lst1:
+    if e in lst2:
+        lst_res.append(e)
+
+print(lst1)
+print(lst2)
+print(lst_res)
+
+# ~ надо(2)
+
+lst1 = [randint(1, 9) for _ in range(10)]
+lst2 = [randint(1, 9) for _ in range(10)]
+
+lst_res = [ e for e in lst1 if e in lst2]
+
+print(lst1)
+print(lst2)
+print(lst_res)
+
