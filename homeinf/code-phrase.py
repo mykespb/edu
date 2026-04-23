d85ce48fc2a0bbc484f77cfb5c4aee85cc8c6367
mykespb <mykespb@gmail.com>
2026-04-23 23:30:02 +0300

    cosint dotty

--- homeinf/code-phrase.py
+++ homeinf/code-phrase.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2024
-# 2026-04-22 2026-04-26 1.0
+# 2026-04-22 2026-04-26 3.0
 # code-phrase.py
 # шифрование текста по кодовой фразе (слову)
 
 import string
 
 abc = string.ascii_letters + string.digits
+# ~ abc = string.ascii_letters + string.digits + " "
+
 print(f"alfabet: {abc}")
 
 
-def encode(text : str, word : str, way : bool = True) -> str:
+def encode1(text : str, word : str, way : bool = True) -> str:
     out = ""
     for i, c in enumerate(text):
         if c in abc:
@@ -24,22 +26,42 @@ def encode(text : str, word : str, way : bool = True) -> str:
     return str(out)
 
 
+def encode2(text : str, word : str, way : bool = True) -> str:
+    out = ""
+    for i, c in enumerate(text):
+        try:
+            if c in abc:
+                if way:
+                    out += abc[ (abc.index(c) + abc.index( word[ i % len(word) ] ) ) % len(abc) ]
+                else:
+                    out += abc[ (abc.index(c) - abc.index( word[ i % len(word) ] ) ) % len(abc) ]
+            else:
+                out += c
+        except:
+            out += c
+    return str(out)
+
+encode = encode2
+
+
 def check(text, word):
 
     print(80 * "=")
-    print(f"{text=}, {word=}")
+    print(f"{text=}\n{word=}")
 
     one = encode(text, word, True)
-    print(f"{one=}")
+    print(f"!{one=}")
     
     two = encode(one, word, False)
-    print(f"{two=}")
+    print(f"!{two=}")
     
 
 def main():
 
     check("Secret text for agent 007", "secret")
     check("Here we go again, and again, see us go!", "fake")
+    check("Скажи мне, кто ты такой, SeeGull", "goer")
+    check("Jonathan Livingston Seagull is an allegorical fable in novella form written by American author Richard Bach and illustrated with black-and-white photographs", "lets start with horizontal flight")
 
     print(80 * "=")
 
---
+++ homeinf/cosine.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2026
+# 2026-04-23 2026-04-23 1.1
+# cosine.py
+
+# ~ Найти число, равное своему косинусу.
+# ~ https://en.wikipedia.org/wiki/Dottie_number
+# ~ https://www.youtube.com/watch?v=wilJNoc7Ubs
+# ~ https://mathworld.wolfram.com/DottieNumber.html
+
+# ~ In mathematics, the Dottie number or the cosine constant is a constant
+# ~ that is the unique real root of the equation cos(x)=x, where the argument of cos is in radians.
+# ~ The decimal expansion of the Dottie number is given by:
+# ~ D = 0.739085133215160641655312087673... (sequence A003957 in the OEIS).
+
+# ~ https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%BE_%D0%94%D0%BE%D1%82%D1%82%D0%B8
+# ~ Число́ До́тти — постоянная, определяемая как вещественное решение уравнения cos(x)=x,
+# ~ где аргумент cos измеряется в радианах. В десятичном представлении число Дотти примерно равно 
+# ~ 0,739085133215...
+
+
+from math import cos
+
+EPS     = 1e-8
+nDottie = 0.739085133215160641655312087673
+
+print(f"  {nDottie=}")
+
+
+def calc():
+
+    old = 0.5
+    while abs((new := cos(old)) - old) > EPS:
+        old = new
+    return new
+    
+cosx = calc()
+
+print(f"     {cosx=}\n{cos(cosx)=}")
+
+
+  # ~ nDottie=0.7390851332151607
+     # ~ cosx=0.7390851372357778
+# ~ cos(cosx)=0.7390851305068246
