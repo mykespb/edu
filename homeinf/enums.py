26101b136a1788f0854293cff23c99ebd24d98ff
mykespb <mykespb@gmail.com>
2026-04-27 21:42:12 +0300

    enums tests with constants

--- homeinf/enums.py
+++ homeinf/enums.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # Mikhail (myke) Kolodin, 2025
-# 2026-04-27 2026-04-27 1.5
+# 2026-04-27 2026-04-27 1.6
 # enums.py
 
 # ~ Пробы перечислений.
 
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, unique, auto
 
 print("""
 # ~ -------------------------------------
@@ -109,6 +109,107 @@ for emp in company:
 # ~ Jame is Role.MANAGER
 # ~ Judith is Role.PROGER
 # ~ Jack is Role.PROGER
+
+print("""
+# ~ -------------------------------------
+# ~ test 3
+# ~ -------------------------------------
+""")
+
+from math import pi
+
+class Planet(Enum):
+    MERCURY = (90.39, 3.3e23)
+    VENUS   = (0.72,  4.87e24)
+    EARH    = (1.,    5.97e24)
+
+    def __init__(self, distance_from_sun, mass):
+        self.distance_from_sun = distance_from_sun
+        self.mass = mass
+
+    @property
+    def density(self):
+        return self.mass / ( 4/3. * pi * (self.distance_from_sun ** 3))
+
+print(Planet.EARH.density)
+print(f"{Planet.EARH.density=}")
+
+
+# ~ 1.425232515387923e+24
+# ~ Planet.EARH.density=1.425232515387923e+24
+
+print("""
+# ~ -------------------------------------
+# ~ test 4
+# ~ -------------------------------------
+""")
+
+@unique
+class UniqueColor(Enum):
+    RED    = 1
+    BLUE   = 2
+    # ~ GREEN  = 3
+    BLACK  = 3
+
+uc = UniqueColor.BLACK
+
+print(f"{UniqueColor.BLACK=}")
+# ~ ValueError: duplicate values found in <enum 'UniqueColor'>: BLACK -> GREEN
+# ~ UniqueColor.BLACK=<UniqueColor.BLACK: 3>
+
+
+print("""
+# ~ -------------------------------------
+# ~ test 5
+# ~ -------------------------------------
+""")
+
+@unique
+class UniqueColor(Enum):
+    RED    = auto()
+    BLUE   = auto()
+    BLACK  = auto()
+
+uc = UniqueColor.BLACK
+
+print(f"{UniqueColor.BLACK=}")
+# ~ UniqueColor.BLACK=<UniqueColor.BLACK: 3>
+
+
+print("""
+# ~ -------------------------------------
+# ~ test 6
+# ~ -------------------------------------
+""")
+
+# ~ https://docs.python.org/3/library/enum.html
+# ~ https://docs.python.org/3/howto/enum.html
+
+Color = Enum('Color', [('RED', 1), ('GREEN', 2), ('BLUE', 3)])
+
+some_color = Color.RED
+
+print(some_color)
+print(some_color in Color)
+
+# ~ Color.RED
+# ~ True
+
+print(dir(Color))
+# ~ ['BLUE', 'GREEN', 'RED', '__class__', '__contains__', '__doc__', '__getitem__', '__init_subclass__', '__iter__', '__len__', '__members__', '__module__', '__name__', '__qualname__']
+
+print(len(Color))
+# ~ 3
+
+print(Color.__members__)
+# ~ {'RED': <Color.RED: 1>, 'GREEN': <Color.GREEN: 2>, 'BLUE': <Color.BLUE: 3>}
+
+print(list(reversed(Color)))
+# ~ [<Color.BLUE: 3>, <Color.GREEN: 2>, <Color.RED: 1>]
+
+print(some_color.name, "=>", some_color.value)
+# ~ RED => 1
+
 
 print("""
 # ~ -------------------------------------
