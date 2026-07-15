cc597bfa7599e1954cd8ed96a76b07493295b9c3
mykespb <mykespb@gmail.com>
2026-07-15 21:57:37 +0300

    3 dots in a line

---
+++ homeinf/dots3line.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2026
+# dots3line.py
+# 2026-07-15 2026-07-16 1.0
+
+
+tests = [
+    [[0., 0.], [1., 1.], [2., 2.]],  # T
+    [[1., 1.], [2., 2.], [3., 3.]],  # T
+    [[0., 0.], [2., 2.], [1., 1.]],  # T
+    [[5., 5.], [1., 1.], [2., 2.]],  # T
+    [[5., 6.], [1., 1.], [2., 2.]],  # F
+    [[6., 2.], [1., 1.], [2., 2.]],  # F
+]
+
+
+EPS = 1e-6
+
+def near(f1, f2):
+    """floats near"""
+
+    return abs(f1 - f2) <= EPS
+    
+
+def inline(test):
+    """run 1 test"""
+
+    test.sort()
+
+    if near(test[0][0], test[1][0]) and near(test[1][0], test[2][0]):
+        return True
+    if near(test[0][1], test[1][1]) and near(test[1][1], test[2][1]):
+        return True
+
+    if near(test[1][0] - test[0][0], 0.) and not near(test[1][1] - test[0][1], 0):
+        return False
+    if near(test[2][0] - test[1][0], 0.) and not near(test[2][1] - test[1][1], 0):
+        return False
+
+    k01 = (test[1][1] - test[0][1]) / (test[1][0] - test[0][0])
+    k12 = (test[2][1] - test[1][1]) / (test[2][0] - test[1][0])
+
+    return near(k01, k12)
+
+
+def main():
+    """run all tests"""
+
+    for num, test in enumerate(tests, start=1):
+        print(f"test #{num:2}: {test} -> {inline(test)}")
+
+
+main()
