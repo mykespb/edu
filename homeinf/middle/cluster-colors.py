62c1a144a613275e2f4bb577db9c7c3918ee60b4
mykespb <mykespb@gmail.com>
2026-07-27 19:04:54 +0300

    clusters - colors

---
+++ homeinf/middle/cluster-colors.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# Mikhail (myke) Kolodin, 2026
+# cluster-colors.py
+# 2026-07-27 2026-07-27 1.0
+
+# ~ Кластеризации
+
+# ~ Есть несколько объектов с наборами заранее заданных параметров или вычисляемых по ним данных.
+# ~ Нужно разделить объкты на группы по наибольшему сходству.
+# ~ Количество групп (кластеров) заранее не известно.
+# ~ Явно заданы цвета (белый, зелёный,...).
+
+
+from collections import defaultdict
+
+
+def cluster(data: str) -> list[str]:
+    """
+    разделить набор данных на кластеры,
+    т.е. на группы близких обеъктов
+    """
+
+    dd = defaultdict(list)
+
+    data = data.strip().split(",")
+    data = map(str.strip, data)
+
+    for para in data:
+        which, what = para.split()
+        dd[which].append(what)
+
+    return dd
+    
+
+def proc(what: str) -> None:
+    print("\n--------------------------------\n")
+
+    print(">>>", what)
+    res = cluster(what)
+    print("<<<")
+
+    for k in res.keys():
+        print(k, ":\n    ", end="")
+        print(*res[k], sep=", ")
+
+    print("\n--------------------------------\n")
+    
+
+proc("""горячий снег, жаркий лёд, весёлая тоска, горькая радость,
+весёлая песня, горячий лист, рабочий посёлок,
+рабочий человек, весёлая покойницкая, жаркий ответ
+""")
+
+proc("""
+white snow, white color, green leaf, blue sea, green apple,
+green grass, white paper
+""")
